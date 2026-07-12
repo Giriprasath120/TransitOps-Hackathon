a9ef997b7941bfde820c4d97f76f96a23c01068f
Giriprasath B <giri.meark@gmail.com>
2026-07-12 14:53:54 +0530

    Added TransitOps core models, menu and security

--- addons/transit_ops/__manifest__.py
+++ addons/transit_ops/__manifest__.py
@@ -1,34 +1,18 @@
-# -*- coding: utf-8 -*-
 {
-    'name': "transit_ops",
+    'name': 'Transit Operations',
+    'version': '1.0',
+    'category': 'Operations',
+    'summary': 'Smart Transport Operations Management',
+    'description': 'Fleet, Driver, Trip, Maintenance and Fuel Management',
+    'author': 'Team TransitOps',
 
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
-
-    'description': """
-Long description of module's purpose
-    """,
-
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
     'depends': ['base'],
 
-    # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'security/ir.model.access.csv',
+        'views/menu.xml',
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
-}
 
+    'installable': True,
+    'application': True,
+}
